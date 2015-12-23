5daa71f544a2efcb87f41493a9c0f1ccf6f33692
Vardan Tadevosyan <tadevosn@jlab.org>
2015-12-23 11:51:21 +0400

    Initial commit. Add gdml coding of scattering chamber.

---
+++ gdml/G01_build/CMakeFiles/load_gdml.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vardan/g4-work/g4.10.01/nps/gdml/G01/load_gdml.cc" "/home/vardan/g4-work/g4.10.01/nps/gdml/G01_build/CMakeFiles/load_gdml.dir/load_gdml.cc.o"
+  "/home/vardan/g4-work/g4.10.01/nps/gdml/G01/src/G01PrimaryGeneratorAction.cc" "/home/vardan/g4-work/g4.10.01/nps/gdml/G01_build/CMakeFiles/load_gdml.dir/src/G01PrimaryGeneratorAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/G4/pro/include/Geant4"
+  "/home/vardan/g4-work/g4.10.01/nps/gdml/G01/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ gdml/G01_build/CMakeFiles/load_gdml.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/load_gdml.dir/load_gdml.cc.o"
+  "CMakeFiles/load_gdml.dir/src/G01PrimaryGeneratorAction.cc.o"
+  "load_gdml.pdb"
+  "load_gdml"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/load_gdml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
